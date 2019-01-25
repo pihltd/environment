38fb0f978d801726128c01bb19de44e83dc4b064
pihltd <pihltd@users.noreply.github.com>
2019-01-25 10:19:21 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,6 +3,6 @@ sudo apt-get -y install \
 build-essential \
 python3-pip \
 mc \
-tig \
-awscli
+tig
 wget https://github.com/tomnomnom/gron/releases/gron-linux-amd64-0.6.0.tgz
+sudo pip3 install awscli
